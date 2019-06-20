f94b17bc7803f71df69d55597b5c43ef6cce3ded
amsv <ammosovaleksey@ya.ru>
2019-06-20 15:28:16 +0300

    Переделал на под ОП

--- script.js
+++ script.js
@@ -1,24 +1,75 @@
-const goods = [
-  { title: "Шорты", price: 150 },
-  { title: "Носки", price: 50 },
-  { title: "Свитер", price: 500 },
-  { title: "Ботинки", price: 1500 },
-  { title: "Футболка", price: 150 },
-  { title: "Платье", price: 5000 },
-  { title: "Плавки", price: 400 },
-  { title: "Шарф", price: 800 },
-  { title: "Шапка", price: 550 },
-  { title: "Сандали", price: 1000 },
-  { title: "Шляпа", price: 1100 },
-  { title: "Пальто", price: 10000 },
-  { title: "Майка", price: 150 },
-  { title: "Пуховик", price: 15000 },
-]
+class GoodsItem {
+  constructor(title = "Товар не выбран", price = 0) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `
+      <div class="good-item">
+      <img src="" alt="">
+      <h3 class="subtitle">${this.title}</h3>
+      <div class="price">${this.price} руб.</div>
+      <button class="btn-add-cart btn">Добавить</button>
+    </div>`;
+  }
+}
 
-const renderGoodsItem = (title = "Товар не выбран", price = "0") => `<div class="good-item"><img src="" alt=""><h3 class="subtitle">${title}</h3><div class="price">${price} руб.</div><button class="btn-add-cart btn">Добавить</button>
-</div>`;
+class GoodsList {
+  constructor (el = ".goods-list") {
+    this.el = el;
+    this.goods = []
+  }
+  fetchGoods() {
+    this.goods = [
+	  { title: "Шорты", price: 150 },
+	  { title: "Носки", price: 50 },
+	  { title: "Свитер", price: 500 },
+	  { title: "Ботинки", price: 1500 },
+	  { title: "Футболка", price: 150 },
+	  { title: "Платье", price: 5000 },
+	  { title: "Плавки", price: 400 },
+	  { title: "Шарф", price: 800 },
+	  { title: "Шапка", price: 550 },
+	  { title: "Сандали", price: 1000 },
+	  { title: "Шляпа", price: 1100 },
+	  { title: "Пальто", price: 10000 },
+	  { title: "Майка", price: 150 },
+	  { title: "Пуховик", price: 15000 },
+	];
+  }
+  render() {
+    const listHtml = this.goods.reduce((acc, good) => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      return acc += goodItem.render();
+    }, '');
+    document.querySelector(this.el).innerHTML = listHtml;
+  }
+  countTotalPrice() {
+      return this.goods.reduce((sumGoods, good) => {
+        return sumGoods + good.price
+      },0)
+  }
+}
 
-const renderGoodsList = ( list = [{title:"Товар не выбран", price: "0"}] ) =>  document.querySelector(".good-list").innerHTML = list.map(item => renderGoodsItem(item.title, item.price)).join("");
-// значение innerHTML строка, а результат map массив. При присвоение происходит привидения массива к строке с соединением елементов массива запятыми. Используя join("") можно элементы массива соеденить "" и превести к строке, а затем эту строку присвоить innerHTML
+class BasketItem {
+  constructor(title = "Товар не выбран", price = 0, count = 0) {
+    this.title = title;
+    this.price = price;
+    this.count = count;
+  }
+}
 
-renderGoodsList(goods);
+class BasketList {
+  constructor () {
+    this.goods = []
+  }
+  addBasketItem() {}//Добавить товар в корзину
+  delBasketItem() {}//Удалить товар из корзины
+  countPriceBasketList() {} // Подсчитать общюю стоимость корзины
+  render() {}// Отрисовка корзины
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.countTotalPrice();
